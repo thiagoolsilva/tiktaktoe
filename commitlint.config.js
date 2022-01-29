1a29064976c1be32c295f85a6811a3045a03604a
thiago lopes da silva <thiagoolsilva@gmail.com>
2022-01-29 15:09:44 -0300

    improv(batect): upgrade batect version to 0.78.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -16,4 +16,7 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type-enum": [2, "always", ["improv","build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]],
+  }
 }
